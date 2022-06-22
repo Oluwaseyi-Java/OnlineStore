a464d30e8360de8cc53550c5838e8a9f4dc81043
Adesunloro Oluwaseyi <amiltontech.co@gmail.com>
2022-06-22 12:44:34 +0100

    Initial commit

---
+++ onlinestore/src/UIDatas.js
@@ -0,0 +1,392 @@
+import React from "react";
+
+
+export const Data = [
+    {
+        id: 1,
+        type: "Food",
+        category: "Breakfast",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Jollof Rice"
+    },
+    {
+        id: 3,
+        type: "Food",
+        category: "Breakfast",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Jollof Rice"
+    },
+    {
+        id: 6,
+        type: "Food",
+        category: "Breakfast",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Jollof Rice"
+    },
+    {
+        id: 6,
+        type: "Food",
+        category: "Breakfast",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Jollof Rice"
+    },
+    {
+        id: 86,
+        type: "Food",
+        category: "Breakfast",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Jollof Rice"
+    },
+    {
+        id: 85,
+        type: "Food",
+        category: "Breakfast",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Jollof Rice"
+    },
+    {
+        id: 43,
+        type: "Food",
+        category: "Launch",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Beans"
+    },
+    {
+        id: 74,
+        type: "Food",
+        category: "Launch",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Beans"
+    },
+    {
+        id: 854,
+        type: "Food",
+        category: "Launch",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Beans"
+    },
+    {
+        id: 7687,
+        type: "Food",
+        category: "Launch",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Beans"
+    },
+    {
+        id: 8796,
+        type: "Food",
+        category: "Launch",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Beans"
+    },
+    {
+        id: 9807,
+        type: "Food",
+        category: "Dinner",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Cake"
+    },
+    {
+        id: 342,
+        type: "Food",
+        category: "Dinner",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Cake"
+    },
+    {
+        id: 987,
+        type: "Food",
+        category: "Dinner",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Cake"
+    },
+    {
+        id: 543,
+        type: "Food",
+        category: "Dinner",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Cake"
+    },
+    {
+        id: 6534,
+        type: "Food",
+        category: "Dinner",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Cake"
+    },
+    {
+        id: 965,
+        type: "Food",
+        category: "Dinner",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Cake"
+    },
+    {
+        id: 3455,
+        type: "Clothing",
+        category: "Men's Wear",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Shirt"
+    },
+    {
+        id: 678654,
+        type: "Clothing",
+        category: "Men's Wear",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Shirt"
+    },
+    {
+        id: 6754,
+        type: "Clothing",
+        category: "Men's Wear",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Shirt"
+    },
+    {
+        id: 3434,
+        type: "Clothing",
+        category: "Men's Wear",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Shirt"
+    },
+    {
+        id: 677564,
+        type: "Clothing",
+        category: "Women's Wear",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Skirt"
+    },
+    {
+        id: 43523,
+        type: "Clothing",
+        category: "Women's Wear",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Skirt"
+    },
+    {
+        id: 564356,
+        type: "Clothing",
+        category: "Women's Wear",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Skirt"
+    },
+    {
+        id: 7675,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Tecno"
+    },
+    {
+        id: 67574,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Tecno"
+    },
+    {
+        id: 54763,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Tecno"
+    },
+    {
+        id: 56564,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Tecno"
+    },
+    {
+        id: 56563,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Infinix"
+    },
+    {
+        id: 453453,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 56563,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 6436,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 43545,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 4365345,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 5643,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 67745,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 56564364,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 346365345,
+        type: "Accesseries",
+        category: "Phone",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "Itel"
+    },
+    {
+        id: 4643634,
+        type: "Accesseries",
+        category: "Laptop",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "HP"
+    },
+    {
+        id: 643634634,
+        type: "Accesseries",
+        category: "Laptop",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "HP"
+    },
+    {
+        id: 46346,
+        type: "Accesseries",
+        category: "Laptop",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "HP"
+    },
+    {
+        id: 65883,
+        type: "Accesseries",
+        category: "Laptop",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "HP"
+    },
+    {
+        id: 43345,
+        type: "Accesseries",
+        category: "Laptop",
+        image: "https://www.google,com/images",
+        reviews: 4,
+        available: true,
+        name: "HP"
+    }
+]
